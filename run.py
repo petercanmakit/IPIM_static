22ea7332559a8485d561c4629df544c5928b415b
petercanmakit <zhangjj2007@hotmail.com>
2017-06-27 17:11:53 -0400

    2017-06-27 end

---
+++ run.py
@@ -0,0 +1,37 @@
+import IPIM
+
+from IPIM.main import app
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+
+        python server.py
+
+    Show the help text using
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+
+
+
+    context = ('ipim.crt', 'ipim.key')
+
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded, ssl_context=context)
+
+
+  run()
