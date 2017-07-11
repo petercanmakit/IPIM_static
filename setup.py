c89013dc6e640bcbb2733a5de9ede6e722453504
petercanmakit <zhangjj2007@hotmail.com>
2017-07-11 15:03:10 -0400

    setup requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     packages=['IPIM'],
     include_package_data=True,
     install_requires=[
-        'Flask>=0.2',
-        'SQLAlchemy>=0.6'
+        'click>=6.7',
+        'Flask>=0.12.1',
+        'Flask-Session>=0.3.1',
+        'Jinja2>=2.9.6',
+        'psycopg2>=2.7.1',
+        'radar>=0.3',
+        'SQLAlchemy>=1.1.4',
+        'Werkzeug>=0.12.1'
     ],
 )
