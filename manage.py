770f0c3b21037e7669553234beb92e717e7461ee
petercanmakit <zhangjj2007@hotmail.com>
2017-07-10 22:52:55 -0400

    mange.py for creating schema and inserting test datasets

---
+++ manage.py
@@ -0,0 +1,130 @@
+import datetime
+import time
+import radar 
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from sqlalchemy.orm import sessionmaker
+
+def createSchema(some_engine):
+	DB_Session = sessionmaker(bind=some_engine)
+	db_session = DB_Session()
+	
+	try:
+		db_session.execute('''
+			DROP TABLE IF EXISTS Users CASCADE;
+			CREATE TABLE Users(
+				Uid int PRIMARY KEY,
+				Name TEXT,
+				Pass TEXT
+			);
+			
+			INSERT INTO Users(uid, name, pass)
+			VALUES (1, 'admin', 'ipim_admin');
+			
+			DROP TABLE IF EXISTS Collisions CASCADE;
+			CREATE TABLE Collisions(
+				Cid bigserial PRIMARY KEY,
+				AccDate Date,
+				State TEXT,
+				City TEXT,
+				Street TEXT,
+				CrossStreet TEXT,
+				Cartype TEXT,
+				TimeOfDay TEXT,
+				PoliceFiled boolean,
+				MedEvaluatedAtScene boolean,
+				TakenToHosFromScene boolean,
+				SeekedCareAfterward boolean,
+				geom geometry NOT NULL,
+				Gender text check (Gender = 'Male' or Gender = 'Female' or Gender = 'not_answered'),
+				Age int,
+				Ethnicity TEXT,
+				Race TEXT,
+				SubmitType TEXT default 'user',
+				SubmittedDate Date default CURRENT_DATE
+			);
+			'''
+		)
+		db_session.commit()
+		print "schema created on success!"
+	except:
+		db_session.rollback()
+		raise
+
+def generateRandomDate(startstr, stopstr):
+	'''
+	start='2000-05-24', stop='2013-05-24T23:59:59'
+	start = datetime.datetime(year=2000, month=5, day=24),
+    stop = datetime.datetime(year=2013, month=5, day=24)
+    "now" -> datetime.datetime.now()
+    return a date string
+	'''
+	startdate = startstr
+	if stopstr == 'now':
+		stopdate = datetime.datetime.now()
+	else:
+		stopdate = stopstr
+	return radar.random_datetime(
+    			start = startdate,
+    			stop = stopdate
+			).isoformat()
+
+def insertTestData(some_engine, some_accdate, some_submitdate):
+	DB_Session = sessionmaker(bind=some_engine)
+	db_session = DB_Session()
+	
+	try:
+		db_session.execute('''
+			INSERT INTO Collisions (
+			AccDate,
+			geom,
+			Gender,
+			SubmitType,
+    		SubmittedDate )
+			VALUES (
+			:acc_date,
+			:geom,
+			:gender,
+			:submittype,
+			:submitteddate )
+			''', {
+			"acc_date": some_accdate,
+			"geom": 'LINESTRING(0 0, 1 1)',
+			"gender": 'not_answered',
+			"submittype": 'test',
+			"submitteddate": some_submitdate
+			}
+		)
+		db_session.commit()
+	except:
+		db_session.rollback()
+		raise
+	
+
+if __name__ == "__main__":
+	import click
+
+	@click.command()
+	@click.option('--create', is_flag=True)
+	@click.option('--insert', is_flag=True)
+	@click.argument('SETS', default=100, type=int)
+	# @click.argument('PORT', default=8111, type=int)
+	def run(create, insert, sets):
+		"""
+		"""
+		SETS = sets
+		DATABASEURI = "postgresql://peter:940611@127.0.0.1/geo" # mac
+		# DATABASEURI = "postgresql://ipim:admin_ipim@127.0.0.1/ipim" # gg cloud linux vm
+		engine = create_engine(DATABASEURI)
+		if create :
+			createSchema(engine)
+		elif insert :
+			for i in range(SETS): 
+				acc_date = generateRandomDate("2017-04-01", 'now')
+				sub_date = generateRandomDate(acc_date , 'now')
+				# print sub_date
+				insertTestData(engine, acc_date, sub_date)
+		else:
+			print "arguments wrong..."
+	
+	run()
