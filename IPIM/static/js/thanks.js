2a412ce5f8513f2bca9d6dd631d05ffee16de0f8
petercanmakit <zhangjj2007@hotmail.com>
2017-07-13 16:01:53 -0400

    email sent tracker

--- IPIM/static/js/thanks.js
+++ IPIM/static/js/thanks.js
@@ -64,11 +64,6 @@ $(document).ready(function () {
           $patchedStyle.remove();
       });
 
-
-          $('#facebookBtn').on('click', function sendFacebookAction() {
-              ga('send', 'social', 'Facebook', 'share', 'https://petercanmakit.github.io/IPIM/');
-              alert("facebook msg sent to ga.")
-          });
           $('#tweetBtn').on('click', function sendTweetAction() {
               ga('send', 'social', 'Twitter', 'tweet', 'https://petercanmakit.github.io/IPIM/');
               alert("twitter msg sent to ga.")
